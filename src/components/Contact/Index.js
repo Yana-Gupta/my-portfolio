b5c8ae080e5b8e7566050269dbb38e2f7a4d76ba
Yana Gupta <yanagupta897@gmail.com>
2023-04-03 20:56:14 +0530

    Button Added

--- src/components/Contact/Index.js
+++ src/components/Contact/Index.js
@@ -1,5 +1,30 @@
-const Contact = () => {
-  return <>hi</>;
-};
+import "./Index.scss"
 
-export default Contact;
+const Contact = () => {
+  return (
+    <div className="min-h-[90vh]">
+      <h1 className="text-2xl font-semibold mx-auto text-center mb-10">
+        Contact Me
+      </h1>
+      <form method="post" className="flex flex-col align-center items-center justify-center">
+        <input
+          type="text"
+          className="px-2 py-2 my-3 h-10 w-96 bg-black"
+          placeholder="Name"
+        />
+        <input
+          type="email"
+          className="px-2 py-2 my-3 h-10 w-96 bg-black"
+          placeholder="Email"
+        />
+        <textarea
+          className="px-2 py-2 my-3 h-36 w-96 bg-black"
+          placeholder="Message"
+        />
+        <button className="send">Send</button>
+      </form>
+    </div>
+  )
+}
+
+export default Contact
